68390ac2ddb811973937dbf82ec4d7108f5544b5
Maiki <maiki.moseley@gmail.com>
2020-12-17 16:08:16 -0600

    add server

---
+++ server.js
@@ -0,0 +1,26 @@
+/* External Modules */
+const express = require("express");
+const methodOverride = require("method-override");
+
+/* Interal Modules */
+const db = require("./models");
+const ctrl = require("./controllers");
+
+/* Instanced Modules */
+const app = express();
+
+/* Configuration */
+const PORT = 4000;
+app.set("view engine", "ejs");
+
+/* Middleware */
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+/* Controllers */
+
+/* Listener */
+app.listen(PORT, function(){
+   console.log(`Live at http://localhost:PORT/`);
+})
