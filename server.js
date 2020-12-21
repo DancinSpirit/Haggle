02af59cf74a73575413516125f419da2670b4727
Maiki <maiki.moseley@gmail.com>
2020-12-21 12:40:01 -0600

    add ruleinfo to rule selector

--- server.js
+++ server.js
@@ -39,12 +39,23 @@ app.use(function(req,res,next){
     next();
 })
 
-/* Check if Signed In */
+/* Check if Already Signed In */
+signinCheck = function(req,res,next){
+    if(!req.session.currentUser){
+        next();
+    } else {
+        if(req.session.currentUser.gamemaster)
+            res.redirect("/gamemaster/players");
+        else
+            res.redirect(`/players/${req.session.currentUser.player}`)
+    }
+}
+/* Check if Not Signed In */
 authRequired = function(req,res,next){
     if(req.session.currentUser){
-      next();
+        next();
     } else {
-      res.redirect("/login");
+        res.redirect("/login");
     }
 }
 /* Check if Gamemaster */
@@ -55,21 +66,38 @@ gamemasterRequired = function(req,res,next){
         res.redirect("/login");
     }  
 }
+/* Check if Not Gamemaster */
+playerRequired = function(req,res,next){
+    if(!req.session.currentUser.gamemaster){
+        next();
+    } else {
+        res.redirect("/login");
+    }  
+}
 
 /* Controllers */
 app.use("/gamemaster", authRequired, gamemasterRequired, controllers.gamemaster);
-app.use("/players", authRequired, controllers.players);
-app.use("/", controllers.auth);
+app.use("/players", authRequired, playerRequired, controllers.players);
 
 /* Home Route */
 app.get("/", function(req, res){
     res.redirect("/login");
 })
 
+/* Logout */ //This is here to get around the login check
+app.get("/logout", async function(req,res){
+    await req.session.destroy();
+    res.redirect("/");
+});
+
+/* Auth Controller */
+app.use("/", signinCheck, controllers.auth);
+
 app.get("*", function(req,res){
     const context = {description:"Page Not Found!", redirect:"/", button: "Home Page"};
     res.render("error",context);
 })
+
 /* Listener */
 app.listen(PORT, function(){
    console.log(`Live at http://localhost:${PORT}/`);
